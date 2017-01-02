57916d12e5880cb79336c61dd6804778747ccb95
Jocelyn Tran <jocelyn.tran1@gmail.com>
2017-01-01 19:46:36 -0800

    Basic functionality

---
+++ app/src/main/java/com/example/jocelyn/tipcalculator/MainActivity.java
@@ -0,0 +1,91 @@
+package com.example.jocelyn.tipcalculator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private double billTotal = 0;
+    private double taxPercentage = 0;
+    private int numPeople = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        SeekBar seekBar = (SeekBar)findViewById(R.id.seekBar);
+        final TextView seekBarValue = (TextView)findViewById(R.id.taxPercent);
+        final TextView taxAmountText = (TextView)findViewById(R.id.taxAmount);
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener(){
+
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress,
+                                          boolean fromUser) {
+                taxPercentage = progress;
+                seekBarValue.setText("Tip Percentage: " + String.valueOf(progress) + "%");
+                taxAmountText.setText("Tax Amount: $" + (double) progress/100*billTotal);
+                updateTotalPayment();
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+            }
+        });
+
+        final EditText billTotalText = (EditText) findViewById(R.id.billTotal);
+        billTotalText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                billTotal = Integer.parseInt( billTotalText.getText().toString() );
+                updateTotalPayment();
+            }
+        });
+
+    }
+
+    public void addPerson( View v ) {
+        TextView peopleText = (TextView)findViewById(R.id.people);
+        numPeople++;
+        peopleText.setText( String.valueOf(numPeople) );
+        updateTotalPayment();
+    }
+
+    public void subtractPerson( View v ) {
+        TextView peopleText = (TextView)findViewById(R.id.people);
+        if( numPeople > 1 ) {
+            numPeople--;
+        }
+        peopleText.setText( String.valueOf(numPeople) );
+        updateTotalPayment();
+    }
+
+
+    public void updateTotalPayment() {
+        TextView totalPayment = (TextView) findViewById(R.id.totalPayment);
+        totalPayment.setText( "Each Person Pays: " +
+                billTotal * ( 1 + (taxPercentage/100) ) / numPeople);
+    }
+
+}
